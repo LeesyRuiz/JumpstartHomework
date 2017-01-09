fcea7cd45013943965780a7685390b06067cb0e3
leesyruiz <leesyruiz@gmail.com>
2017-01-08 22:10:47 -0800

    working model - needs pretty

--- CandyMachine.rb
+++ CandyMachine.rb
@@ -1,18 +1,26 @@
-# #Input amount of $
-# puts "Hello! My name is Willy Wonka... here is a list of everything i'm offering from my Chocolate Factory today "
-#
-# puts "A. Three-Course Dinner Chewing Gum : $1\n\r"
-# puts "B. Wonka Bar : $2\n\r"
-# puts "C. Everlasting Gobstoppers : $3\n\r"
-# puts "D. Cavity-Filling Caramels : $4\n\r"
-# puts "E. Rainbow Drops : $5\n\r"
-
 ######### ruby candymachine.rb
+
+#Input amount of $
+puts "Hello! My name is Willy Wonka... here is a list of everything i'm offering from my Chocolate Factory today "
+
+Candy_Hash = {
+   "A" => { "Three-Course Dinner Chewing Gum" => 1},
+   "B" => {"Wonka Bar" => 2},
+   "C" => { "Everlasting Gobstoppers" => 3},
+   "D" => {"Cavity Filling Caramels" => 4},
+   "E" => {"Rainbow Drops" => 5},
+ }
+
+#use hash to print in future
+puts "A. Three-Course Dinner Chewing Gum : $1\n\r"
+puts "B. Wonka Bar : $2\n\r"
+puts "C. Everlasting Gobstoppers : $3\n\r"
+puts "D. Cavity-Filling Caramels : $4\n\r"
+puts "E. Rainbow Drops : $5\n\r"
 
 
 
 array = ["A", "B", "C", "D", "E"]
-
 
   begin
   print "Make a selection?"
@@ -32,15 +40,31 @@ array = ["A", "B", "C", "D", "E"]
 
 #input budget
 print "How much money do you have to spend?"
-budget = gets.chomp.to_i
+budget = gets.chomp.to_f
 
 puts "You have $#{budget} to spend at my candy shop... "
 
 #Cal budget - cost of thee candy...
-#budget = 4
-cost = 5
-if budget >=  cost
-  print "Here is your candy "
+
+
+
+candy_name = ""
+candy_cost = ""
+selection = Candy_Hash[response.upcase]
+selection.each { |name , cost|
+  candy_name = name
+  candy_cost = cost.to_f
+ }
+
+
+# puts candy_name
+# puts candy_cost
+
+
+if budget >=  candy_cost
+  change = budget.to_f - candy_cost
+
+  print "Enjoy your #{candy_name}. Your change is #{change}"
 else
   print "Pick something cheaper "
 end
